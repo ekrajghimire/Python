b63af5f5465189fe40921397d9d787a03cdb3a8b
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-05-24 23:55:04 +0530

    Data Types in Python

---
+++ data_types.py
@@ -0,0 +1,27 @@
+a = 5
+print (a)
+print(type(a))
+
+b = True
+print (b)
+print(type(b))
+
+c = "test"
+print (c)
+print(type(c))
+
+d = [1,2,3,4,5]
+print (d)
+print(type(d))
+
+e = {1,2,3,4,5}
+print (e)
+print(type(e))
+
+f = {1:"a",2:"b"}
+print (f)
+print(type(f))
+
+g = (5,6,7,8)
+print (g)
+print(type(g))
